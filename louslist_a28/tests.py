943604870f64b17a6185450975abdbcda01c25dc
nimajraz <82776123+nimajraz@users.noreply.github.com>
2022-10-06 13:03:24 -0400

    initial testing

---
+++ louslist_a28/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+# Create your tests here.
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+#from .models import Question
+from django.urls import reverse
+# def create_question(question_text, days):
+#     """
+#     Create a question with the given `question_text` and published the
+#     given number of `days` offset to now (negative for questions published
+#     in the past, positive for questions that have yet to be published).
+#     """
+#     time = timezone.now() + datetime.timedelta(days=days)
+#     return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class GoogleLoginViewTests(TestCase):
+    def test_page_error(self):
+        """
+        If page results in 404 error, test fails
+        """
+        response = self.client.get(reverse('louslist_a28:accounts'))
+        self.assertEquals(response.status_code, 200)
--- louslist_a28/urls.py
+++ louslist_a28/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+app_name = 'louslist_a28'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls'), name='accounts'),
 
 ]
