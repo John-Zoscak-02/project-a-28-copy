3ab73b8592f650600aa0331f5b85a852f9a9d1ea
kevinmchung <kevinmchung888@gmail.com>
2022-10-20 12:18:19 -0400

    added course list to department pages

--- home/urls.py
+++ home/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('calendar/', views.CalendarView.as_view(), name='calendar'),
     path('<int:pk>', views.CourseDetailView.as_view(), name='course_detail'),
-    path('about-us/', views.AboutUsView.as_view(), name='about-us')
-    
+    path('about-us/', views.AboutUsView.as_view(), name='about-us'),
+    path('department/<str:dept>', views.DeptDetailView.as_view(), name='dept_detail')
 ]
