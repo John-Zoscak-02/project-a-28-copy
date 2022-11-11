e4aa7983c9ce2ecc9b3a4bbcda2af02bcd3a4751
nimajraz <82776123+nimajraz@users.noreply.github.com>
2022-11-11 14:28:37 -0500

    models for comments

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Comment
 class ProfileModelForm(forms.ModelForm):
     class Meta:
         model = Profile
@@ -9,3 +9,15 @@ class SearchForm(forms.Form):
     department = forms.CharField(label='Department Abbreviation', widget=forms.TextInput(attrs={'class': 'form-control'}))
     course_number = forms.CharField(label='Course Number', widget=forms.TextInput(attrs={'class': 'form-control'}))
     days = forms.CharField(label='Days', widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+""" class CommentForm(forms.ModelForm):
+    content = forms.CharField(label ="", widget = forms.Textarea(
+    attrs ={
+        'class':'form-control',
+        'placeholder':'Comment about schedule here',
+        'rows':4,
+        'cols':50
+    }))
+    class Meta:
+        model = Comment
+        fields = ('content', ) """
--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
+from email.policy import default
 from pyexpat import model
-import profile
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -77,6 +77,17 @@ class Relationship(models.Model):
     def __str__(self):
         return f"{self.sender}-{self.receiver}-{self.status}"
 
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add = True)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    content = models.TextField()
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return 'Comment {} by {} on {}'.format(self.content, self.user.username, self.date)
+
 class Department(models.Model):
     subject = models.CharField(max_length=4)
 
@@ -87,7 +98,7 @@ class Course(models.Model):
     catalog_number = models.CharField(max_length=4)
     description = models.CharField(max_length=64)
     units = models.CharField(max_length=1)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.department} {self.catalog_number} - {self.description} ({self.units} units)"
