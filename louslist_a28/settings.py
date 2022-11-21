8ecd1e7002965852df6ce5b7759ef4fa74f0dacc
nimajraz <82776123+nimajraz@users.noreply.github.com>
2022-11-21 00:06:48 -0500

    Revert "change db to posgresql"
    This reverts commit 8dfff34e720965ccd3a7e5f307ffdd91a1d7f1b5.

--- louslist_a28/settings.py
+++ louslist_a28/settings.py
@@ -105,12 +105,8 @@ WSGI_APPLICATION = 'louslist_a28.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Cooper221341.',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
